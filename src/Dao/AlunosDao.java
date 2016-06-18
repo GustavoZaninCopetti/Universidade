4667818678cf56ff869750fc57f74aaeb64f10e2
Bruno Lopes <Bruno Lopes@Ultragamer.mshome.net>
2016-06-18 00:48:39 -0300

    Tabelas feitas e alguma olhada na conexão

---
+++ src/Dao/AlunosDao.java
@@ -0,0 +1,226 @@
+package Dao;
+
+import GetsSets.Alunos;
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+//As exceções devem ser propagadas para a camada de apresentação, ou seja, deve ser utilizado throws em cada um dos métodos
+public class AlunosDao {
+
+    public void delete(Alunos alunos) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = Conexao.getConnection();
+            String sql = "delete from Aluno where codigo = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, alunos.getAlunos_codigo());
+            ps.execute();
+
+            conn.commit();
+        } catch(SQLException e) {
+            System.out.println("ERRO: " + e.getMessage());
+
+            if(conn != null){
+                try {
+                    conn.rollback();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+
+
+        } finally {
+            if( ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+            if(conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+        }
+    }
+
+    //update pessoa set nome = ? where id = ?
+    
+    
+    public void insert(Alunos alunos) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = Conexao.getConnection();
+            String sql = "insert into produtos (codigo,nome,cidade,sexo) values(?,?,?,?)";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, alunos.getAlunos_codigo());
+            ps.setString(2, alunos.getAlunos_nome());
+            ps.setString(3, alunos.getAlunos_cidade());
+            ps.setString(4, alunos.getAlunos_estado());
+            ps.execute();
+
+            conn.commit();
+
+        } catch(SQLException e) {
+            System.out.println("ERRO: " + e.getMessage());
+
+            if(conn != null){
+                try {
+                    conn.rollback();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+
+        } finally {
+            if( ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+            if(conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+        }
+    }
+
+    public void update(Alunos alunos) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = Conexao.getConnection();
+            String sql = "update Aluno set nome = ? where codigo = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, alunos.getAlunos_nome());
+            ps.setInt(2, alunos.getAlunos_codigo());
+            ps.execute();
+
+            conn.commit();
+        } catch(SQLException e) {
+            System.out.println("ERRO: " + e.getMessage());
+
+            if(conn != null){
+                try {
+                    conn.rollback();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+
+
+        } finally {
+            if( ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+            if(conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+        }
+    }
+
+
+
+    public List<Alunos> getAll() {
+        List<Alunos> lista = new ArrayList<Alunos>();
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = Conexao.getConnection();
+            String sql = "select codigo, nome from Aluno";
+            ps = conn.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                Integer codigo = rs.getInt(1);
+                String nome = rs.getString(2);
+                Alunos p = new Alunos();
+                p.setAlunos_codigo(codigo);
+                p.setAlunos_nome(nome);
+                lista.add(p);
+            }
+        } catch(SQLException e) {
+            System.out.println("ERRO: " + e.getMessage());
+        } finally {
+            if( ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+            if(conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+        }
+        return lista;
+    }
+
+    public Alunos getAlunos(Integer codigo) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = Conexao.getConnection();
+            String sql = "select codigo, nome from Aluno where codigo = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, codigo);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                Integer cod = rs.getInt(1);
+                String nome = rs.getString(2);
+                Alunos p = new Alunos();
+                p.setAlunos_codigo(cod);
+                p.setAlunos_nome(nome);
+                return p;
+            }
+        } catch(SQLException e) {
+            System.out.println("ERRO: " + e.getMessage());
+        } finally {
+            if( ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+            if(conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    System.out.println("ERRO: " + ex.getMessage());
+                }
+            }
+        }
+        return null;
+    }
+
+
+
+}
---
+++ src/Dao/Conexao.java
@@ -0,0 +1,23 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("org.hsqldb.jdbcDriver");
+            //conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/bancodb", "sa", "");
+            conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/univerdb", "sa", "");
+        } catch (SQLException e) {
+            System.out.println("Problemas ao conectar no banco de dados");
+        } catch (ClassNotFoundException e) {
+            System.out.println("O driver não foi configurado corretametne");
+        }
+
+        return conn;
+    }
+}
